002bd02c6434e9fb3bea8aeb3aa1d6f76f1b45bb
Fatih <fatihk.96brs@gmail.com>
2025-02-10 15:48:28 +0300

    Initial Commit

---
+++ ViperProject/ViperProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViperProjectApp.swift
+//  ViperProject
+//
+//  Created by Fatih Kenarda on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ViperProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ViperProjectTests/ViperProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViperProjectTests.swift
+//  ViperProjectTests
+//
+//  Created by Fatih Kenarda on 10.02.2025.
+//
+
+import Testing
+@testable import ViperProject
+
+struct ViperProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
